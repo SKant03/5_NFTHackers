c1a3e71e0abfde011a89037888ba495270bd42a4
Ajay <sumitsrawat2003@gmail.com>
2024-03-06 16:10:11 +0530

    front-end

---
+++ client/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import logo from './assets/logo.png'
+import search from './assets/search.svg'
+
+import button from './assets/button.svg'
+import './App.css'
+
+function App() {
+
+  
+  return (
+<div style={{height:'800px',display:'flex',flexDirection:'column',backgroundColor:'#03151B',gap:'7rem'}} >
+
+
+<div className='nav' style={{ height:'10%',display:'flex',justifyContent:'space-between',alignItems:'center',marginTop:'2%'}}>
+
+
+ <img src={logo}  width={'15%'} />
+  <div className='nav' style={{backgroundColor:'white',height:'50%',width:'32%',borderRadius:25,display:'flex',alignItems:'center'}}>
+   <img src={search} alt="" srcset="" style={{width:'7%',marginLeft:'4%'}}  />
+   <input placeholder='Search' style={{outline:'none',borderColor:'transparent',fontSize:20}}>
+   </input>
+
+
+  </div>
+  <div style={{width:'20%',display:'flex',justifyContent:'space-around',height:'100%',alignItems:'center'}}>
+  <div style={{backgroundColor:"#23843E",height:'50%',width:'40%',borderRadius:15,display:'flex',justifyContent:'center',alignItems:'center'}}>
+   
+  <p style={{fontFamily:'sans-serif',color:'white',fontWeight:700,fontSize:'20px'}}> SIGNUP</p>
+
+  </div>
+  <div style={{backgroundColor:"#23843E",height:'50%',width:'40%',borderRadius:15,display:'flex',justifyContent:'center',alignItems:'center'}}>
+  <p style={{fontFamily:'sans-serif',color:'white',fontWeight:700,fontSize:'20px'}}> LOGIN</p>
+
+  </div>
+  </div>
+</div>
+<div className='middle' style={{display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center'}}>
+  <p className='paragraph'>
+  WANT to CREATE NFT’s?    
+  </p>
+  <p className='paragraph'>Do It with few clicks!</p>
+  <p style={{color:'white',fontFamily:'sans-serif',fontSize:'30px'}}>
+  x users created their own nft till now,  </p>
+  <p style={{color:'white',fontFamily:'sans-serif',margin:'auto',fontSize:'30px'}}> 
+try a new way to create nft</p>
+
+<img style={{marginTop:'5%'}} src={button}  width={'20%'} alt="" srcset="" />
+
+
+</div>
+
+</div>
+  )
+}
+
+export default App
